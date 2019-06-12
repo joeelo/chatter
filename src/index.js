084d603aa45b02eb45acc05abe33d1dd898d5d94
Joseph <josephclorenzo@gmail.com>
2019-06-12 09:34:11 -0400

    installing npm modules excluded ENV files and the like

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
-app.listen(process.env.PORT, () => {
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
     console.log(`listening to env PORT`)
 })
